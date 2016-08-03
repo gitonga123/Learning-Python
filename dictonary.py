db70bb9fcc2673515482bdc77dca4c3df6c08616
Gitonga123 <mutwiridanielsci@gmail.com>
2016-08-03 14:04:42 +0300

    Python Dictionaries

--- dictonary.py
+++ dictonary.py
@@ -10,5 +10,34 @@ if 'Daniel' in phonebook:
 	print(phonebook['Daniel'])
 	phonebook['Joe'] = '555-0123'
 	phonebook['Chris'] = '555-444'
-	print(phonebook)
+	
+	for key in phonebook:
+		print key, phonebook[key]
+		pass
+	#Getting the Number of Elements in a Dictonary
+	num_items = len(phonebook)
+	print num_items
+
+
+	#further Manipulation
+	test_scores = {'Kyla':[88,92,100],'sophie':[95,74,81],'Ethan':[70,88,91,45],'Luis':[70,56,34]}
+	print test_scores
+	test_sophie = test_scores['Kyla']
+	print len(test_sophie)
+	total =0 
+	for value in test_sophie:
+		total +=value
+		pass
+	print total
 	pass
+
+#get can be used instead[] to get the value of a key
+value = phonebook.get('Joe', 'Entry not found')
+print value
+
+for key, value in phonebook.items():
+	print key, value
+
+for key in phonebook.keys():
+	print key
+
