f2db504c9db4866d9324eb3ff8348dfb175f19c6
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-02-23 09:34:50 +0300

    Introduction to Python

---
+++ intro.py
@@ -0,0 +1,18 @@
+print "Origins of python: 1989"
+
+print "Create by  Guido Van Rossum"
+
+print "Got it's name from the Monty Python's Flying Circus - The famous  British Comedy troupe of the eighties"
+
+print "The main motivation was the ease at which it was eable to extend and that it could support multiple platforms"
+
+print "Through out the nineties, python continued to grow, users were concerned that it was entirely dependent on Van Rossum "
+
+print "The solution was to make Python  open-source, available to all, and to facilitate that Python 2.0 was released in 2000 by the BeOpen"
+
+print "Python Lab team, wth the idea beign that it was more community oriented and a lot more transparent in the development process"
+
+print "It's Repo was moved to SourceFourge giving more people 'Write' access to the CVS tree and a much easier way for bufs to reported and patches submitted"
+print "It's a general purpose programming language, used to do al sort of things."
+print "Its readability, Library, and Community makes it more prefential among other competetive programming languages"
+
---
+++ intro2.py
@@ -0,0 +1,7 @@
+print ("Hello, World!")
+
+def my_function():
+	print ("Hello World!")
+
+
+my_function()
