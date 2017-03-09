722238dc127a2f5eacb10abe3b18ef96c3a05997
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-03-09 10:35:12 +0300

    Converting PDF to EXCEL

--- convert_pdf.py
+++ convert_pdf.py
@@ -3,5 +3,7 @@ import pdftables_api
 
 # go to https://pdftables.com/pdf-to-excel-api
 # Great work
-c = pdftables_api.Client('07bkfkqw170a')
+#07bkfkqw170a
+#curl https://pdftables.com/api/remaining?key=ualbk77994ho
+c = pdftables_api.Client('ualbk77994ho')
 c.xlsx('Sportpesa __ Get in the Game.pdf', 'output.xlsx')
