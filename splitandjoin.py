0834955afb224d036998752bbe08337b2e035dbc
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-05-04 15:46:09 +0300

    String Manipulation

---
+++ splitandjoin.py
@@ -0,0 +1,13 @@
+strings = ['I','A','Proud','Kenyan']
+
+print(' '.join(strings))
+
+spam = '''	Dear Alice,
+How have you been? I am fine.
+There is a container in the fridge
+that is labeled "Milk Experiment".
+Please do not drink it.
+Sincerely,
+Bob'''
+
+print(spam.split('\n'))
---
+++ upperlower.py
@@ -0,0 +1,33 @@
+spam = "Hello World"
+span = spam.upper()
+spar = spam.lower()
+
+print span
+print spar
+
+print('How are you ?')
+
+feeling = raw_input()
+
+if feeling.lower()== 'great' or feeling.lower() == 'fine':
+	print(' I feel great too.')
+else:
+	print('I hope the rest of you day is good')
+
+print spam.islower()
+
+
+print spam.isupper()
+
+print span.isupper()
+
+print spar.islower()
+
+#other methods include
+
+"""
+	isdecimal(), isalpha,isalnum, istitle,isspace
+
+	startswith,endwith, 
+	the join() and split() methods.
+"""
