1a3b3a4da0e5aae635df18eca6044da6c65e6c64
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-05-04 14:08:12 +0300

    Multiline Strings with Triple Quotes

---
+++ strings1.py
@@ -0,0 +1,10 @@
+print('''
+	Dear Alice,
+	Eve's cat has been arrested by the police. This
+	is as result of mischevous crimes related to catnapping,
+	cat burglary, and extortion.
+
+	Sincerely,
+	Bob'''
+
+	)
