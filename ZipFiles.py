efca8a428d6ea31b9db20bb24e9c8e39415b983c
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-05-23 16:30:38 +0300

    Reading Zip File

---
+++ ZipFiles.py
@@ -0,0 +1,13 @@
+
+import zipfile,os
+
+print("The Current Working Directory is" + os.getcwd())
+
+folderzip = zipfile.ZipFile('theme_darkly-9.0.0.3.zip')
+
+print(folderzip.namelist())
+
+spamInfo = folderzip.getinfo('theme_darkly/README.md')
+print(spamInfo.file_size)
+
+folderzip.close()
