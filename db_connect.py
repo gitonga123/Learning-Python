a5d34321beaaeb5b7a6436a476a0a2a8803b6e56
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-05-23 11:59:42 +0300

    Python and MySQL using MySQLdb and mysql.connector

---
+++ db_connect.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# This Python file uses the following encoding: utf-8
+import os, sys
+import MySQLdb
+hostname = 'localhost'
+username = 'root'
+password = 'daniel123'
+database = 'holdon'
+
+# Simple routine to run a query on a database and print the results:
+def doQuery( conn ) :
+    cur = conn.cursor()
+
+    cur.execute( "SELECT team_name, home FROM TABLE3" )
+
+    for team_name, home in cur.fetchall() :
+        print team_name, home
+
+
+print "Using MySQLdb…"
+
+myConnection = MySQLdb.connect( host=hostname, user=username, passwd=password, db=database )
+doQuery( myConnection )
+myConnection.close()
+
+# print "Using pymysql…"
+# import pymysql
+# myConnection = pymysql.connect( host=hostname, user=username, passwd=password, db=database )
+# doQuery( myConnection )
+# myConnection.close()
+
+# print "Using mysql.connector…"
+# import mysql.connector
+# myConnection = mysql.connector.connect( host=hostname, user=username, passwd=password, db=database )
+# doQuery( myConnection )
+# myConnection.close()
