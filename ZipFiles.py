fb75fbc6daf1c67b16f2d141c97b54ba11976c85
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-05-23 16:44:15 +0300

    Readin, Extracting and Compressing,anding file in Folders, Zip File using the ZipFile Module

--- ZipFiles.py
+++ ZipFiles.py
@@ -7,10 +7,10 @@ folderzip = zipfile.ZipFile('theme_darkly-9.0.0.3.zip')
 
 print(folderzip.namelist())
 
-# spamInfo = folderzip.getinfo('theme_darkly')
-# print(spamInfo.file_size)
+spamInfo = folderzip.getinfo('theme_darkly')
+print(spamInfo.file_size)
 
-# print(spamInfo.compress_size)
+print(spamInfo.compress_size)
 
 folderzip.extractall(os.path.join(os.getcwd(),'files','dark'))
 
