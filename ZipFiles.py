351087ec78dc029395142682df7aa42f4140d0b0
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-05-23 16:43:23 +0300

    Reading Zip File using the ZipFile Module

--- ZipFiles.py
+++ ZipFiles.py
@@ -7,9 +7,11 @@ folderzip = zipfile.ZipFile('theme_darkly-9.0.0.3.zip')
 
 print(folderzip.namelist())
 
-spamInfo = folderzip.getinfo('theme_darkly/README.md')
-print(spamInfo.file_size)
+# spamInfo = folderzip.getinfo('theme_darkly')
+# print(spamInfo.file_size)
 
-print(spamInfo.compress_size)
+# print(spamInfo.compress_size)
+
+folderzip.extractall(os.path.join(os.getcwd(),'files','dark'))
 
 folderzip.close()
